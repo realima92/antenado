5910dd6fdeb364e745078849a1aeb0e5e6d9b872
Renato Lima <renato.lima92@gmail.com>
2018-07-29 16:35:20 -0300

    Correções

---
+++ SistemaAntenado/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaAntenado.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
